cc23e53c15144e2e85d04f224f835cda315a2ebb
Ben Baron <ben@einsteinx2.com>
2012-08-26 00:40:54 -0700

    Fixed bug in email validation with capital letters

--- EX2Kit/Categories/Foundation/NSString/NSString+Validation.m
+++ EX2Kit/Categories/Foundation/NSString/NSString+Validation.m
@@ -13,6 +13,7 @@
 // Code from http://cocoawithlove.com/2009/06/verifying-that-string-is-email-address.html
 - (BOOL)validateEmail
 {
+    DLog(@"validating: %@", self);
 	NSString *emailRegEx =
     @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
     @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
@@ -23,7 +24,7 @@
     @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 	
 	NSPredicate *regExPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx];
-	return [regExPredicate evaluateWithObject:self];
+	return [regExPredicate evaluateWithObject:[self lowercaseString]];
 }
 
 @end
