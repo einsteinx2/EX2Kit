f87b3fec2e0f286368a590f787b4a9b5e03a0f84
Justin <jhill.d@gmail.com>
2013-01-03 16:26:11 -0500

    - ifdef'd out things that would prevent EX2Kit from building properly on OS X - (decryption/encryption headers, NSURL+SkipBackupAttribute category)

--- EX2Kit/Components/EX2Components.h
+++ EX2Kit/Components/EX2Components.h
@@ -9,8 +9,11 @@
 #ifndef EX2Kit_EX2NonUIComponents_h
 #define EX2Kit_EX2NonUIComponents_h
 
+#ifdef IOS
 #import "EX2FileDecryptor.h"
 #import "EX2FileEncryptor.h"
+#endif
+
 #import "EX2RingBuffer.h"
 #import "EX2SimpleConnectionQueue.h"
 
