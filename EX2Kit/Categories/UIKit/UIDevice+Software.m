496678af681aa10d50efb43fc135d173a0ed836b
Benjamin Baron <ben@einsteinx2.com>
2013-06-18 13:16:31 +0200

    - Added isOnPhoneCall method to UIDevice

--- EX2Kit/Categories/UIKit/UIDevice+Software.m
+++ EX2Kit/Categories/UIKit/UIDevice+Software.m
@@ -8,6 +8,8 @@
 
 #import "UIDevice+Software.h"
 #import <sys/sysctl.h>
+#import <CoreTelephony/CTCallCenter.h>
+#import <CoreTelephony/CTCall.h>
 
 @implementation UIDevice (Software)
 
@@ -54,6 +56,17 @@
     
     return isJailbroken;
 #endif
+}
+
+- (BOOL)isOnPhoneCall
+{
+    CTCallCenter *callCenter = [[CTCallCenter alloc] init];
+    for (CTCall *call in callCenter.currentCalls)
+    {
+        if (call.callState != CTCallStateDisconnected)
+            return YES;
+    }
+    return NO;
 }
 
 @end
