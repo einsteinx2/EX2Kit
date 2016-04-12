0b6250080dcf8aa364aefa605c7b4f30c5921fdd
Jwix <doumet.alex@gmail.com>
2016-04-12 17:31:26 +0300

    Import encryptor and decryptor on tv

--- EX2Kit/Components/EX2Components.h
+++ EX2Kit/Components/EX2Components.h
@@ -9,12 +9,8 @@
 #ifndef EX2Kit_EX2NonUIComponents_h
 #define EX2Kit_EX2NonUIComponents_h
 
-#ifdef IOS
 #import "EX2FileDecryptor.h"
 #import "EX2FileEncryptor.h"
-#import "EX2NetworkIndicator.h"
-#import "EX2Reachability.h"
-#endif
 
 #import "EX2RingBuffer.h"
 #import "EX2SimpleConnectionQueue.h"
@@ -22,5 +18,10 @@
 #import "EX2ActionQueue.h"
 #import "EX2Action.h"
 #import "EX2ActionBlock.h"
+
+#ifdef IOS
+    #import "EX2NetworkIndicator.h"
+    #import "EX2Reachability.h"
+#endif
 
 #endif
